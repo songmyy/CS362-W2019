e2990e9716aca24489c1972e6a8390320fcccad7
MyungJin Song <songmy@flip3.engr.oregonstate.edu>
2019-01-27 22:13:01 -0800

    refactored code and introduced bugs

--- projects/songmy/dominion/dominion.c
+++ projects/songmy/dominion/dominion.c
@@ -643,6 +643,83 @@ int getCost(int cardNumber)
   return -1;
 }
 
+void adventurerAction(int currentPlayer, struct gameState *state){
+  int drawntreasure=0;
+  int temphand[MAX_HAND];
+  int cardDrawn;
+  int z=0;
+        while(drawntreasure<2){
+  if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
+    shuffle(currentPlayer, state);
+  }
+  drawCard(currentPlayer, state);
+  cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
+  if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+  //BUG-drawntreasure++ was changed to drawntreasure--
+  //whenever adventurer is played, program will hang 
+    drawntreasure--;
+  else{
+    temphand[z]=cardDrawn;
+    state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+    z++;
+  }
+      }
+      while(z-1>=0){
+  state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
+  z=z-1;
+      }
+}
+
+void smithyAction(int currentPlayer, struct gameState *state, int handPos){
+  int i;
+    //BUG-changed i from 0 to 1
+    //player gains one less card than expected
+      //+3 Cards
+      for (i = 1; i < 3; i++)
+  {
+    drawCard(currentPlayer, state);
+  }
+      
+      //discard card from hand
+      discardCard(handPos, currentPlayer, state, 0);
+}
+
+void villageAction(struct gameState *state, int handPos){
+    int currentPlayer = whoseTurn(state);
+        //+1 Card
+      drawCard(currentPlayer, state);
+  //BUG - numActions + 2 was changed to just numActions
+  //players +2 action effect is now gone   
+      //+2 Actions
+      state->numActions = state->numActions;
+      
+      //discard played card from hand
+      discardCard(handPos, currentPlayer, state, 0);
+}
+
+void great_hallAction(struct gameState *state, int handPos){
+    int currentPlayer = whoseTurn(state);
+        //+1 Card
+      drawCard(currentPlayer, state);
+      
+      //+1 Actions
+      state->numActions++;
+      
+      //discard card from hand
+      discardCard(handPos, currentPlayer, state, 0);
+}
+
+void outpostAction(struct gameState *state, int handPos){
+    int currentPlayer = whoseTurn(state);
+    //BUG - ++ from outpostPlayed++ was removed
+    //outpost card would have no action even if played
+        //set outpost flag
+      state->outpostPlayed;
+      
+      //discard card
+      discardCard(handPos, currentPlayer, state, 0);
+}
+
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
   int i;
@@ -655,9 +732,10 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
   int tributeRevealedCards[2] = {-1, -1};
   int temphand[MAX_HAND];// moved above the if statement
-  int drawntreasure=0;
-  int cardDrawn;
-  int z = 0;// this is the counter for the temp hand
+  // moved due to refactor of advernturer
+  // int drawntreasure=0;
+  // int cardDrawn;
+  // int z = 0;// this is the counter for the temp hand
   if (nextPlayer > (state->numPlayers - 1)){
     nextPlayer = 0;
   }
@@ -667,25 +745,8 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   switch( card ) 
     {
     case adventurer:
-      while(drawntreasure<2){
-	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	  shuffle(currentPlayer, state);
-	}
-	drawCard(currentPlayer, state);
-	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-	  drawntreasure++;
-	else{
-	  temphand[z]=cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  z++;
-	}
-      }
-      while(z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
-      }
-      return 0;
+    adventurerAction(currentPlayer, state);
+    return 0;
 			
     case council_room:
       //+4 Cards
@@ -829,26 +890,12 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case smithy:
-      //+3 Cards
-      for (i = 0; i < 3; i++)
-	{
-	  drawCard(currentPlayer, state);
-	}
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+    smithyAction(currentPlayer, state, handPos);
+    return 0;
 		
     case village:
-      //+1 Card
-      drawCard(currentPlayer, state);
-			
-      //+2 Actions
-      state->numActions = state->numActions + 2;
-			
-      //discard played card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+    villageAction(state, handPos);
+    return 0;
 		
     case baron:
       state->numBuys++;//Increase buys by 1!
@@ -902,15 +949,8 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case great_hall:
-      //+1 Card
-      drawCard(currentPlayer, state);
-			
-      //+1 Actions
-      state->numActions++;
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+    great_hallAction(state, handPos);
+    return 0;
 		
     case minion:
       //+1 action
@@ -1156,12 +1196,8 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case outpost:
-      //set outpost flag
-      state->outpostPlayed++;
-			
-      //discard card
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+    outpostAction(state, handPos);
+    return 0;
 		
     case salvager:
       //+1 buy
